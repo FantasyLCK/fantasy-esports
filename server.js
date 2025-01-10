06a082d040446962eedd8def30f0482497a767b3
dawnsaram <gunhj0625@gmail.com>
2025-01-10 14:58:19 +0900

    Add mongodb library

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-app.use(express.static(__dirname + '/public'));
+const { MongoClient } = require('mongodb')
+
+let db
+const url = 'mongodb+srv://admin:admin00@cluster0.dmb9t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+new MongoClient(url).connect().then((client) => {
+  console.log('Successfully connected to MongoDB')
+  db = client.db('db')
+  app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000')
+})
+}).catch((err) => {
+  console.log(err)
+})
+
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
-});
+})
 
-app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000')
-});
+app.get('/players', (req, res) => {
+    db.collection('players').insertOne({
+    })
+})
