e4c40284ffa9a75a64570bc7462d15840e8aff47
dawnsaram <gunhj0625@gmail.com>
2025-01-13 13:39:14 +0900

    Edit login page

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const { MongoClient } = require('mongodb')
 
 let db
 const url = 'mongodb+srv://admin:admin00@cluster0.dmb9t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
 new MongoClient(url).connect().then((client) => {
   console.log('Successfully connected to MongoDB')
   db = client.db('database')
   app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000')
+    console.log('Express is running on http://localhost:3000')
 })
 }).catch((err) => {
   console.log(err)
@@ -18,5 +19,5 @@ new MongoClient(url).connect().then((client) => {
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+    res.sendFile(__dirname + '/main/index.html')
 })
