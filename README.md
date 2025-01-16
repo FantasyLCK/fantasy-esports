1a083157bb02f527f53610cadc92bdf9ad9dba4b
dawnsaram <gunhj0625@gmail.com>
2025-01-16 15:11:05 +0900

    Add detail/list.ejs

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # Fantasy LCK - Web 개발 코드
 
-## 파일 구조 (node_modules 제외)
+## 디렉토리 구조 (node_modules 제외)
 ```
 fantasy-esports
 ├─ README.md
-├─ json
+├─ Information.md // 프로젝트 정보 파일
+├─ json // 기본 DB 폴더
 │  ├─ players.json
 │  └─ teams.json
 ├─ package-lock.json
 ├─ package.json
-├─ public
+├─ public // 정적 파일 폴더 (css, js, Sass, 이미지)
 │  ├─ Sass
 │  │  └─ main.scss
 │  ├─ css
@@ -21,7 +22,31 @@ fantasy-esports
 │  │  └─ main-bg.png
 │  └─ js
 │     └─ bootstrap.bundle.min.js
-├─ server.js
-└─ views
+├─ server.js // 서버 파일
+└─ views // html 페이지 폴더 (ejs 사용)
    └─ index.ejs
 ```
+
+# 개발 예정 기능
+- 로그인/회원가입
+  - 구글/디스코드 계정으로 로그인
+- 로스터 확인
+  - 등록한 TOP/JGL/MID/BOT/SPT 선수 확인
+  - 팀 케미스트리/팀순위 부스트/POG 부스트 사항 확인
+- 선수 검색
+  - 선수 이름으로 검색
+  - 포지션/소속팀 필터 기능
+- 알림창
+  - 패치노트/공지사항 알림
+- 친구 관리
+  - 서버 내 유저 간 친구 추가/관리
+- 매치업(친선/일반)
+  - 친선: 등록된 친구와 매치업
+  - 일반: 서버 내에서 랜덤 유저와 매치업
+- 랭킹
+  - 팀 가치 총합/매치업 승률 순위 표시
+- 커뮤니티 채팅
+  - 서버 내 이용자들과 채팅
+  - '내 로스터' 공유 기능
+- 튜토리얼
+- 계정 정보 페이지
--- server.js
+++ server.js
@@ -28,5 +28,40 @@ new MongoClient(url).connect().then((client) => {
 
 // 메인 페이지 불러오기
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+  res.render('index.ejs')
 })
+
+app.get('/list/:position', async (req, res) => {
+  try {
+    const position = req.params.position; // position 값 가져오기
+    let query = {};
+
+    // position 값이 존재하면 쿼리 조건 추가
+    if (position) {
+      query = { position: position.toUpperCase() }; // 대소문자 무시
+    }
+
+    const result = await db.collection('players').find(query).toArray(); // 쿼리 실행
+    res.render('list.ejs', { result: result }); // 결과를 렌더링
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error'); // 에러 처리
+  }
+});
+
+app.get('/detail/:name', async (req, res) => {
+  try {
+    const playerName = req.params.name; // name 가져오기
+    const result = await db.collection('players').findOne({ name: playerName });
+
+    if (!result) {
+      // 에러 처리
+      return res.status(404).send(`<h1>Player "${playerName}" not found</h1>`);
+    }
+    res.render('detail.ejs', { result: result });
+  } catch (err) {
+    // 에러 처리
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
